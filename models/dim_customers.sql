3f18cf6d70757edcfdc15cdc7bb5fd4a2b79e19e
cquarles@clickup.com <cquarles@clickup.com>
2021-11-15 00:55:08 +0000

    ref_actual_update

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -4,24 +4,15 @@
 
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
+    SELECT *
+    FROM {{ref('stg_customers')}}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
+    SELECT *
+    FROM {{ref('stg_orders')}}
 
 ),
 
