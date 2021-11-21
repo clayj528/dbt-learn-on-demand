51c9284742ddc20c73a9fe98345e782fbb346426
Clayton Quarles <cquarles@clickup.com>
2021-11-21 00:44:37 +0000

    updated_training_materials

---
+++ models/marts/core/fct_orders.sql
@@ -0,0 +1,32 @@
+
+with orders as  (
+    SELECT * 
+    FROM {{ ref('stg_orders' )}}
+),
+
+payments as (
+    SELECT * 
+    FROM {{ ref('stg_payments') }}
+),
+
+order_payments as (
+    select
+       order_id
+     , sum(CASE WHEN status = 'success' then amount end) as amount
+    from payments
+    group by 1
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
+)
+
+select * from final
